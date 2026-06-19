1c706ccf01dbe795c9fa25d9b50d77a701e0a862
Alireza Pourgholam <alireza.pourgholam444@gmail.com>
2026-06-19 10:26:02 +0330

    fix: update basePath and font paths to new repo name Alirewa-PortfolioWeb

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-const basePath = isGitHubPages ? '/MyPersonal-Portfolio-Website' : ''
+const basePath = isGitHubPages ? '/Alirewa-PortfolioWeb' : ''
 
 const nextConfig = {
   output: 'export',
